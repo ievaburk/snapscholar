f445cc48dafa91e770e181dcc15725fab086de82
Ieva Burk <eburk19@cmc.edu>
2019-07-13 00:18:31 -0700

    tryna add my AI and comp viz stories

---
+++ ai-story/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 5000;
+var path = require('path');
+
+app.use(express.static('public'));
+app.get('/',function(req, res) {
+	res.sendFile(path.join(__dirname, "/public", "/story1ai.html"));
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
